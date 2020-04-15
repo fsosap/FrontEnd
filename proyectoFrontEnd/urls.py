e95c1cc770a60934e8439c0570d9293c1c449860
fsosap <fsosap@eafit.edu.co>
2020-04-15 17:05:23 -0500

    FrontEnd que ahora encuentra la ruta

--- proyectoFrontEnd/urls.py
+++ proyectoFrontEnd/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from measure import views as measure_views
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', measure_views.measure, name='measure')
 ]
