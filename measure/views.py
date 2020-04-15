a74800fe1b0ce82d674838801ce148eee36cf012
fsosap <fsosap@eafit.edu.co>
2020-04-15 18:29:22 -0500

    entrega final FrontEnd finiquitado

--- measure/views.py
+++ measure/views.py
@@ -2,7 +2,23 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
-
+import requests
 # Create your views here.
 def measure(request):
-    return render(request, "measure/measure.html")
+    # Verifica si hay un parámetro value en la petición GET
+    if 'value' in request.GET:
+        value = request.GET['value']
+        # Verifica si el value no esta vacio
+        if value:
+            # Crea el json para realizar la petición POST al Web Service
+            args = {'type': 'ph', 'value': value}
+            response = requests.post('http://127.0.0.1:8000/hygrometer/', args)
+            # Convierte la respuesta en JSON
+            measure_json = response.json()
+
+    # Realiza una petición GET al Web Services
+    response = requests.get('http://127.0.0.1:8000/hygrometer/')
+    # Convierte la respuesta en JSON
+    measures = response.json()
+    # Rederiza la respuesta en el template measure
+    return render(request, "measure/measure.html", {'measures': measures})
